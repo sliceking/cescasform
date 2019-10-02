af2f0b9857d25df9a8dfd0f85b37adb6d04da745
svwielga4 <svwielga4@gmail.com>
2019-10-02 12:35:53 -0700

    adding some more hookups from the fron to the back and validations

--- stan/functions.php
+++ stan/functions.php
@@ -24,8 +24,36 @@ if ( is_page_template( 'Move form' ) ) {
         'FROM_WP',
         [
             'ajax_url' => admin_url( 'admin-ajax.php' ),
-            'nonce' => wp_create_nonce( 'move-n-nonce' ),
+            'nonce' => wp_create_nonce( 'move-n' ),
         ]
     );
+}
+
+// the actions submitted from the form are connected to our function that handles the submission
+add_action( 'wp_ajax_move_in_form_submission', 'move_in_form_submission' );
+add_action( 'wp_ajax_nopriv_move_in_form_submission', 'move_in_form_submission' );
+function move_in_form_submission(){
+    // we check to make sure the nonce is valid
+    check_ajax_referer( 'move-n', 'nonce' );
+
+    // do some sanitization first
+
+    // do some validation on the back end
+
+    // add the extra data to the request before sending
+    // Submission URL: https://cptest.move-n.com/api/Lead/LeadInfo
+    // Sender_ID: 849b2101c11b171ca1cb65b27d1119127ee38f2c
+    // Parent_ID: 224
+
+    // send request off to the 3rd party service
+    $res = wp_remote_post();
+
+    // check response
+
+    // if response is good return a
+    wp_send_json_success();
+
+    // if its bad
+    wp_send_json_error();
 
 }
--- stan/moven.js
+++ stan/moven.js
@@ -15,23 +15,55 @@ var formHandler = (function($){
     var typeOfService = $('#type_of_service');
     var submitButton = $('#submit_button');
 
-    var validateInputs = function(){
+    /**
+     * checks all the inputs to make sure they are in a good state
+     * @return errors object|false
+     */
+    var checkInputs = function(){
         // do some validation on the input values
     }
 
+    /**
+     * updates the DOM to notify a user about improper form data
+     */
     var displayErrors = function(){
         // when some data is not present
         // give some tips to the user on what to do
         // to put the form in a good state
     }
 
-    var submitForm = function(){
+    /**
+     * Grabs all the form values and extra data needed for form submission
+     */
+    var packageData = function(){
+        return {
+            action: 'move_in_form_submission',
+            nonce: FROM_WP.nonce,
+            firstName: firstName.val(),
+            lastName: lastName.val(),
+            homePhone: homePhone.val(),
+            cellPhone: cellPhone.val(),
+            email: email.val(),
+            address1: address1.val(),
+            address2: address2.val(),
+            city: city.val(),
+            state: state.val(),
+            zip: zip.val(),
+            note: note.val(),
+            prospectFirstName: prospectFirstName.val(),
+            prospectLastName: prospectLastName.val(),
+            typeOfService: typeOfService.val(),
+        };
+    }
+
+    var submitForm = function(data){
         // if button isnt disabled
         // submit the information to OUR server
         $.ajax({
             method: "POST",
             url: FROM_WP.ajax_url, //localize the ajax url from wp
-            data: , // package all the data up first
+            dataType: 'json',
+            data: data, // package all the data up first
             success: function(){
                 // some success actions
             },
@@ -43,15 +75,17 @@ var formHandler = (function($){
 
     var handleSubmit = function(){
         // check for violations
-        var hasErrors = validateInputs();
+        var hasErrors = checkInputs();
         // if some violations display Errors
         if (hasErrors) {
+            // early return because there are errors
             displayErrors();
             return;
         }
 
-        // if none submit
-        submitForm();
+        // if everything is good, pack up the data and submit
+        var data = packageData();
+        submitForm(data);
     }
 
     var init = function(){
